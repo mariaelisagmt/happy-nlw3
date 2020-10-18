cae334f54d6d8ec42f7d81d2f8e2330b4abb0064
Maria Elisa Gomes <mariaelisagmt@gmail.com>
2020-10-18 15:17:37 -0300

    Estruturação da estilização de popup no mapa

--- public/js/page-orphanages.js
+++ public/js/page-orphanages.js
@@ -1,15 +1,27 @@
-//Mapa 
-const map = L.map('mapid').setView([51.505, -0.09], 13);
+//Map
+const map = L.map('mapid').setView([-3.6876675,-40.3582159], 15);
 
-L.tileLayer(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
-    {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }
-).addTo(map);
+//create and add tilteLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-L
-.marker([51.5, -0.09])
+//Create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+});
+
+const popup = L.popup ({
+    closeButton: false,
+    className: 'map-popoup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanages"><img src="./public/images/arrow-white.svg"></a>');
+
+
+
+L.marker([-3.6876675,-40.3582159], {icon: icon})
 .addTo(map)
-.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-.openPopup();
+.bindPopup(popup);
+
